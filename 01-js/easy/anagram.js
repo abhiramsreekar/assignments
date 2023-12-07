c9f635e53d7c40345fdda3a2e4256240a54fea1f
Abhiram Sreekar <83418471+abhiramsreekar@users.noreply.github.com>
2023-12-07 22:42:16 +0530

    Anagram completed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -4,8 +4,24 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
-function isAnagram(str1, str2) {
+let sortString = (s) => {
+  return s.split("").sort().join("");
+};
 
+function isAnagram(str1, str2) {
+  let s1=str1.toLowerCase();
+  let s2=str2.toLowerCase();
+  let news1=s1;
+  let news2=s2;
+  // for(let i=0;i<s1.length;i++){
+  //   if(s1[i]>='a' && s1[i]<='z') news1+=s1[i]; 
+  // }
+  // for(let i=0;i<s2.length;i++){
+  //   if(s2[i]>='a' && s2[i]<='z') news2+=s2[i]; 
+  // }
+  if(sortString(news1)==sortString(news2)) return true;
+  return false;
 }
 
+// console.log(isAnagram('School MASTER', 'The ClassROOM'));
 module.exports = isAnagram;
