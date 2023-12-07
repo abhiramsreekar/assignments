7796a968a23b712b20265b60b6d177630cdf7c95
Abhiram Sreekar <83418471+abhiramsreekar@users.noreply.github.com>
2023-12-07 20:20:56 +0530

    palindrome done

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,7 +4,20 @@
 */
 
 function isPalindrome(str) {
+  let s=str.toLowerCase();
+  let news="";
+  for(let i=0;i<s.length;i++){
+    if(s[i]>='a' && s[i]<='z') news+=s[i];
+  }
+  // console.log(news);
+  for(let i=0;i<news.length;i++)
+  {
+    if(news[i]!=news[news.length-i-1]) return false;
+    // console.log(news[i],news[news.length-i-1]);
+  }
   return true;
 }
+
+console.log(isPalindrome('Eva, can I see bees in a cave?'));
 
 module.exports = isPalindrome;
