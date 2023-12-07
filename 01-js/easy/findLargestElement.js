9f1f957f6188ea77d086b2aba3bce3c0c33cf7d1
Abhiram Sreekar <83418471+abhiramsreekar@users.noreply.github.com>
2023-12-07 20:06:42 +0530

    init

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,11 @@
 */
 
 function findLargestElement(numbers) {
-    
+    let large=numbers[0];
+    for(let i=0;i<numbers.length;i++){
+        if(numbers[i]>large) large=numbers[i];
+    }
+    return large;
 }
 
 module.exports = findLargestElement;
